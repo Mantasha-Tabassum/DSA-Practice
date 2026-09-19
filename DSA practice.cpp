a48af30419fc36ca29c88670867c51a20919a7a2
Mantasha Tabassum <mantashatabassum87@gmail.com>
2026-09-19 17:52:46 +0530

    Add DSA practice files

---
+++ DSA practice.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[n-1];
+
+    for(int i=0; i<n-1; i++)
+    {
+        cin >> arr[i];
+    }
+    
+    int sum = 0;
+    
+    for(int i=0; i<n-1; i++)
+    {
+        sum = sum + arr[i];
+    }
+      
+      int expectedSum = n * (n+1)/2;
+      int missing = expectedSum - sum;
+      
+            {
+                cout << missing;
+            }
+return 0;
+}
